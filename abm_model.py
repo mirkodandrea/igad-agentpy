ac122c48383bee0f0209443c8dbe641b55c39bc6
mirkodandrea <mirko.dandrea@gmail.com>
2022-12-14 17:53:55 +0100

    implemented one year resolution

--- abm_model.py
+++ abm_model.py
@@ -13,7 +13,7 @@ from numpy.random import random, normal, poisson, pareto
 # parametro per scalare la distribuzione dell'income (crescente -> distribuizione più piatta)
 ALPHA_INCOME = 0.5
 # parametro di povertà
-POVERTY_LINE = 2
+POVERTY_LINE = 1
 # 1KM 
 MAX_DISTANCE = 0.002
 # minimum flood depth to be considered as a flood
@@ -21,21 +21,29 @@ FLOOD_DAMAGE_THRESHOLD = 10
 FLOOD_DAMAGE_MAX = 1000
 FLOOD_FEAR_MAX = 300
 
-#%%
-def get_events(initial_row=0, stride=20):
-    events = []
-    df_floods = pd.read_csv('data_2/IGAD/SD_EventCalendar.csv')
-    for idx, row in df_floods.iloc[initial_row:stride].iterrows():
-        with rio.open(f'data_2/IGAD/Maps/SD_30mHazardMap_{row.EventID:0>4d}.tif') as f:
-            flood_data = f.read(1)
+MAX_YEARS = 100
 
-        events.append(
-            dict(
-                data=flood_data, 
-                year=row.Year, 
-                interarrival_time=row.InterarrivalTime,
-                rio_object=f
-        ))
+#%%
+def get_events(initial_year, stride):
+    events = {}
+    df_floods = pd.read_csv('data_2/IGAD/SD_EventCalendar.csv')
+    df_floods = df_floods.query('Year >= @initial_year and Year < @initial_year+@stride')
+
+    for year, group in df_floods.groupby('Year'):
+        for idx, row in group.iterrows():
+            with rio.open(f'data_2/IGAD/Maps/SD_30mHazardMap_{row.EventID:0>4d}.tif') as f:
+                flood_data = f.read(1)
+
+            if year not in events:
+                events[year] = []
+
+            events[year - initial_year].append(
+                dict(
+                    data=flood_data, 
+                    year=row.Year, 
+                    interarrival_time=row.InterarrivalTime,
+                    rio_object=f
+            ))
     
     return events
 # %%
@@ -54,22 +62,29 @@ class HouseHold(ap.Agent):
     def fix_damage(self):
         """
         fix damage for current household
-        use interarrival_time to calculate how much damage can be fixed
         """
         t = self.model.t -1
         if t==0: return
-        
-        interarrival_time = self.p.events[t]['interarrival_time']
 
-        # it takes 1 year to fix 100% of damage if income is >=10
-        # else every unit of income less than 10 increases the time to fix the damage by 1.2 month
-        recovery_time = 365  \
-                        if self.income > 10 \
-                        else 365 + (10 - self.income) * 36.5
+        if self.income <= POVERTY_LINE:
+            return
 
-        recovery = interarrival_time/recovery_time
+        if self.displaced:
+            # it takes 3 year to fix 100% of damage if income is >=10
+            recovery_time = 3*365    
+        else:
+            recovery_time = 365    
+
+        # every unit of income less than 10 increases the time to fix the damage by 6 month
+        if self.income < 10:
+            recovery_time += (10 - self.income) * 180
+
+        recovery = 365/recovery_time
         
         self.damage = np.clip(self.damage - recovery, 0, 1)
+        
+        if self.damage < 0.25 and self.displaced:
+            self.displaced = False
 
     def update_awareness(self, previous_awareness):
         """
@@ -90,12 +105,18 @@ class HouseHold(ap.Agent):
         - damage occurs if flood value is > 10mm
         - damage is proportional to flood value: 1m -> 100% damage
         """
-        t = self.model.t - 1
-        event = self.p.events[t]
-        r = event['rio_object']
-        flood_data = event['data']
-        row, col = r.index(*self.position)
-        flood_value = flood_data[row, col]
+        t = self.model.t
+        events = self.p.events[t]
+        flood_value = np.NaN
+
+        for event in events:
+            r = event['rio_object']
+            flood_data = event['data']
+            row, col = r.index(*self.position)
+            flood_value = np.nanmax(
+                [flood_data[row, col],
+                flood_value]
+            )
 
         # flood value is in millimeters
         if flood_value > FLOOD_DAMAGE_THRESHOLD:
@@ -114,10 +135,6 @@ class HouseHold(ap.Agent):
         if self.displaced: return
 
         # if household income is below poverty line, it cannot be displaced
-        if self.income <= POVERTY_LINE:
-            self.displaced = False
-            return
-
         if self.damage >= 0.65:
             self.displaced = True
         elif self.damage >= 0.1:
@@ -129,8 +146,7 @@ class HouseHold(ap.Agent):
         t = self.model.t -1
         if t==0: return
         
-        interarrival_time = self.p.events[t]['interarrival_time']
-        fear_recovery = interarrival_time/365        
+        fear_recovery = 0.2
         self.fear = np.clip(self.damage - fear_recovery, 0, 1)
 
 
@@ -170,13 +186,17 @@ class Model(ap.Model):
     def step(self):
         """ Call a method for every agent. """
         previous_awareness = [h.awareness for h in self.households]
-
-        self.households.fix_damage()
         self.households.update_awareness(previous_awareness)
         self.households.update_fear()
-        flood_values = self.households.do_flood()        
-        self.households.set_fear(flood_values)
-        self.households.displace()
+
+        if self.t in self.p.events:
+            # there is one or more event this year
+            flood_values = self.households.do_flood()        
+            self.households.set_fear(flood_values)
+            self.households.displace()
+        else:
+            # no event this year
+            self.households.fix_damage()
 
 
     def update(self):
@@ -187,7 +207,7 @@ class Model(ap.Model):
         self.households.record('fear')
         self.households.record('displaced')
         
-        if (self.t + 1) >= len(self.p.events):
+        if (self.t + 1) >= MAX_YEARS:
             self.stop()
 
     def end(self):
@@ -200,6 +220,7 @@ def animation_plot_single(m, ax):
     print(f"{m.t}")
     ax.set_title(f"t={m.t}")
     displaced_idx = np.array(m.households.displaced)
+    poors_idx = np.array(m.households.income) < POVERTY_LINE
 
     pos = m.domain.positions.values()
     pos = np.array(list(pos)).T  
@@ -209,6 +230,9 @@ def animation_plot_single(m, ax):
     ax.scatter(*pos[:, ~displaced_idx], c=damages[~displaced_idx],
                cmap='viridis', marker='s', s=3 + 25*awarenesses[~displaced_idx])
     ax.scatter(*pos[:, displaced_idx], c=damages[displaced_idx], cmap='viridis', marker='x', s=3 + 25*awarenesses[displaced_idx])
+
+    ax.scatter(*pos[:, poors_idx], facecolors='none',
+               edgecolors='g', marker='o', s=30)
 
     cx.add_basemap(ax, crs='epsg:4326',
                    source=cx.providers.OpenStreetMap.Mapnik)
@@ -227,7 +251,7 @@ def animation_plot(model):
 
 
 settlements = gpd.read_file('data_2/settlements_with_price.gpkg').to_crs(epsg=4326)
-events = get_events(stride=30)
+events = get_events(initial_year=0, stride=MAX_YEARS)
 n_households = len(settlements)
 prices = settlements['price'].values
 lons = settlements.geometry.centroid.x
@@ -252,15 +276,23 @@ params = dict(
 model = Model(params)
 animation_plot(model)
 #%%
-df = model.output['variables']['HouseHold']
-df['damage'].unstack().mean(axis=1).plot()
-#%%
-df['displaced'].unstack().mean(axis=1).plot()
+def plot_stats(model, variable):
+    df = model.output['variables']['HouseHold']
+    var = df[variable].unstack().mean(axis=0)
+    ax = var.plot.bar()
+    max_y = var.max()*1.1
+    ax.set_ylim(0, max_y)
+    events_years = events.keys()
+    ax.stem(events_years, [max_y]*len(events_years), 'r')
+    ax.set_title(variable)
+    plt.show()
+
+
+plot_stats(model, 'damage')
+plot_stats(model, 'displaced')
+plot_stats(model, 'awareness')
+plot_stats(model, 'fear')
+plot_stats(model, 'perception')
+
+
 # %%
-df['awareness'].unstack().mean(axis=1).plot()
-# %%
-df['fear'].unstack().mean(axis=1).plot()
-# %%
-df['perception'].unstack().mean(axis=1).plot()
-# %%
-#sometimes_displaced = df['displaced'].groupby('obj_id').any()
